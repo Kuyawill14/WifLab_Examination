518ec7ddac8a4c4aba77e173627cedcb1e1c6f77
Mwill14 <62843284+Mwill14@users.noreply.github.com>
2022-10-06 10:54:26 +0800

    Third

--- suggest.js
+++ suggest.js
@@ -3,14 +3,14 @@ require("dotenv").config(); //require dotenv to access env file;
 
 process.argv.shift()  // skip node.exe
 process.argv.shift()  // skip name of js file
-const PlaceName = process.argv.join(' '); //add quote in string
-FindSuggestionPlace(PlaceName); //Call function FindSuggestionPlace with paramater
+const PlaceName = process.argv.join(' ') //add quote in string
+FindSuggestionPlace(PlaceName) //Call function FindSuggestionPlace with paramater
 
 
 function FindSuggestionPlace(PlaceName){
-    console.log(`Query Suggestion for "${PlaceName}":`);
+    console.log(`Query Suggestion for "${PlaceName}":`)
     const Api_Key = process.env.GOOGLE_API_KEY //get Api key from .env
-    console.log(Api_Key);
+    console.log(Api_Key)
 
 
 }
